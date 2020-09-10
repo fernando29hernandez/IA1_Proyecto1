83fb6d23e61336ca873362a99dd1cef8e74ac547
Fernando Hernandez <hernandezfernando18982012@gmail.com>
2020-09-09 23:08:13 -0600

    limpieza codigo

--- controllers/ConsultaController.js
+++ controllers/ConsultaController.js
@@ -5,14 +5,14 @@ var reglas = require("../data/Contexto.js");
 var datos = require("../data/datos.js");
 var program = reglas;
 session.consult(program);
-consultaController.index = function (req, res) {
-  res.render("../views/index");
-};
 var cadena = "";
 var contador = 0;
 var arregloenvio = [];
-// Obtener argumento de Node.js: nodejs ./script.js item
-function hola(temp) {
+
+
+
+
+function busquedaGenerica(temp) {
   try {
     console.log("|" + temp.toString());
     let cadena_animal = temp.toString();
@@ -27,49 +27,21 @@ function hola(temp) {
         arregloenvio.unshift(animal);
       }
     });
-    //console.log(arregloenvio)
   } catch (e) {}
 }
 
-function hola2(temp) {
-  if(contador==0){
-  console.log(temp.toString());
+function busquedaEspecifica(temp) {
+  if (contador == 0) {
+    console.log(temp.toString());
     let cadena_animal = temp.toString();
-    //cadena_animal = cadena_animal.replace(new RegExp("false.", "g"), "");
-    cadena_animal = cadena_animal.split(";")
+    cadena_animal = cadena_animal.split(";");
     cadena_animal = cadena_animal[0];
     arregloenvio = cadena_animal;
-    contador=1;
+    contador = 1;
   }
 }
 
 function procesar(consulta_cadena, nombre) {
-  // Cargar programa
-  var ejemplo = // Productos
-    "item(id(1), name(bread))." +
-    "item(id(2), name(water))." +
-    "item(id(3), name(apple))." +
-    // Tiendas
-    "shop(id(1), name(tau), location(spain))." +
-    "shop(id(2), name(swi), location(netherlands))." +
-    // Inventario
-    "stock(item(1), shop(1), count(23), price(0.33))." +
-    "stock(item(2), shop(1), count(17), price(0.25))." +
-    "stock(item(2), shop(2), count(34), price(0.31))." +
-    "stock(item(3), shop(2), count(15), price(0.45)).";
-
-  /*let item = "water";
-  session.query(
-    "item(id(ItemID), name(" +
-      item +
-      ")), stock(item(ItemID), shop(ShopID), _, price(Price)), shop(id(ShopID), name(Shop), _)."
-  );
-
-  session.answers((x) => hola("pregunta:" + pl.format_answer(x)));
-  session.query("item(id(2),name(" + item + ")).");
-  
-  session.answers((x) => hola("resp: " + pl.format_answer(x)));
-  */
   if (nombre != "nada") {
     datos.forEach(function (animal) {
       if (animal.nombre == nombre) {
@@ -82,22 +54,30 @@ function procesar(consulta_cadena, nombre) {
             consulta_cadena +
             "."
         );
-        session.answers((x) => hola2("" + pl.format_answer(x)));
+        session.answers((x) => busquedaEspecifica("" + pl.format_answer(x)));
       }
     });
   } else {
     session.query("animal(nombre(N),id(I))" + consulta_cadena + ".");
-    session.answers((x) => hola("" + pl.format_answer(x)));
+    session.answers((x) => busquedaGenerica("" + pl.format_answer(x)));
   }
 }
+
+
+consultaController.index = function (req, res) {
+  res.render("../views/index");
+};
+
 
 consultaController.list = function (req, res) {
   procesar(req.body.cadena, req.body.nombrebuscado);
   res.send({ resultado: arregloenvio });
   arregloenvio = [];
   cadena = "";
-  contador=0;
+  contador = 0;
 };
+
+
 consultaController.view = function (req, res) {
   let indice = req.params.id;
   datos.forEach(function (animal) {
@@ -106,4 +86,6 @@ consultaController.view = function (req, res) {
     }
   });
 };
+
+
 module.exports = consultaController;
